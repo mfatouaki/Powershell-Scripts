7d1629ca52e0b0c2e772f64784280788b5c958fe
Mehdi Fatouaki <123072235+mfatouaki@users.noreply.github.com>
2023-01-19 11:33:35 +0100

    Add files via upload

---
+++ computer object id.ps1
@@ -0,0 +1,10 @@
+﻿Install-module -name msonline -force 
+Connect-MsolService
+$CHGList = Import-Csv -path "C:\temp\MachineNames.csv"
+$ObjectID = @(
+    @{ Name = 'version:v1.0'; Expression = { $_.'ObjectId' } }
+)
+'*'| Select-Object -Property $ObjectID |Export-CSV -Path "C:\temp\testx.csv" -NoTypeInformation -Append -Force
+ForEach ($displayName in $CHGList) {
+    Get-MsolDevice -Name $displayName.displayName | Select-Object -Property $ObjectID |Export-CSV -Path "C:\temp\BulkUploadtemplate.csv" -NoTypeInformation -Append
+}
