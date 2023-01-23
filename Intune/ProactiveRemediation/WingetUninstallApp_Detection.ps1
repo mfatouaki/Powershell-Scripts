91d6a2df0f508936e29ce059106c6eed828532de
Mehdi Fatouaki <123072235+mfatouaki@users.noreply.github.com>
2023-01-23 15:18:48 +0100

    Update WingetUninstallApp_Detection.ps1

--- Intune/ProactiveRemediation/WingetUninstallApp_Detection.ps1
+++ Intune/ProactiveRemediation/WingetUninstallApp_Detection.ps1
@@ -1,16 +1,18 @@
 # Check if winget is already installed
-$winget = Get-Command winget
+$winget = Get-Command winget -ErrorAction SilentlyContinue
 
 
 # If winget is not installed, install it
 if (!$winget) {
-    Invoke-WebRequest https://github.com/Axlfc/get-winget/blob/main/install-winget.ps1 -OutFile install-winget.ps1
+$MyLink = "https://github.com/microsoft/winget-cli/releases/download/v1.3.431/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
+Invoke-WebRequest -Uri $MyLink -OutFile winget-installer.msixbundle
+Add-AppxPackage -Path .\winget-installer.msixbundle -ForceUpdateFromAnyVersion -ForceTargetApplicationShutdown
 }
 
 
 # Check if the app is currently installed
 $appName = "mozilla firefox"
-$app = winget list --name $appName 
+$app = winget list --name $appName --accept-source-agreements
 
 
 if ($app -like "No installed package found*") {
@@ -21,8 +23,3 @@ if ($app -like "No installed package found*") {
     Write-Output "$appName Is Installed."
     Exit 1
 }
- catch{
-    $errMsg = $_.Exception.Message
-    Write-Error $errMsg
-    exit 1
-} 
