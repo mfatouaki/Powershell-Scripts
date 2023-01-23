bc649e7197d5a16ab324b16001673d8f878d9292
Mehdi Fatouaki <123072235+mfatouaki@users.noreply.github.com>
2023-01-23 16:37:07 +0100

    Update WingetUninstallApp_Detection.ps1

--- Intune/ProactiveRemediation/WingetUninstallApp_Detection.ps1
+++ Intune/ProactiveRemediation/WingetUninstallApp_Detection.ps1
@@ -4,9 +4,18 @@ $winget = Get-Command winget -ErrorAction SilentlyContinue
 
 # If winget is not installed, install it
 if (!$winget) {
-$MyLink = "https://github.com/microsoft/winget-cli/releases/download/v1.3.431/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
-Invoke-WebRequest -Uri $MyLink -OutFile winget-installer.msixbundle
+
+#Winget dependency install
+$dependency = "https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx"
+Invoke-WebRequest -Uri $dependency -OutFile Microsoft.VCLibs.x64.14.00.Desktop.appx
+Add-AppxPackage -Path .\Microsoft.VCLibs.x64.14.00.Desktop.appx -ForceUpdateFromAnyVersion -ForceTargetApplicationShutdown
+
+
+#Winget Install
+$WingetDLLink = "https://github.com/microsoft/winget-cli/releases/download/v1.3.431/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
+Invoke-WebRequest -Uri $WIngetDLLink -OutFile winget-installer.msixbundle
 Add-AppxPackage -Path .\winget-installer.msixbundle -ForceUpdateFromAnyVersion -ForceTargetApplicationShutdown
+
 }
 
 
