2791b32181402962e9d45d564e8eb0ff725625af
Mehdi Fatouaki <123072235+mfatouaki@users.noreply.github.com>
2023-06-13 15:58:45 +0100

    Add files via upload

---
+++ Intune/ProactiveRemediation/CreatePartition_Detection.ps1
@@ -0,0 +1,9 @@
+﻿#Detection script
+$DriveLetter = "D"
+$partition = get-partition -driveletter $DriveLetter -ErrorAction SilentlyContinue
+
+if( $partition) { Write-Host "Disk Partition Exist.";
+Exit 0}
+else 
+{Write-Host "Disk partition doesn't exist" 
+Exit 1 }
