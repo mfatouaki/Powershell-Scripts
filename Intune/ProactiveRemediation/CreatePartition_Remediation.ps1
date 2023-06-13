0698e87f53a65fc5b7f51e78733cfc3bc4bd15f4
Mehdi Fatouaki <123072235+mfatouaki@users.noreply.github.com>
2023-06-13 15:59:02 +0100

    Add files via upload

---
+++ Intune/ProactiveRemediation/CreatePartition_Remediation.ps1
@@ -0,0 +1,11 @@
+﻿#Select Partition
+Get-Partition -DriveLetter C
+
+#Resize the choosen partition
+Resize-Partition -DriveLetter C -Size (237GB)
+
+#Create a new partition and format disk
+New-Partition -DiskNumber 0 -UseMaximumSize -DriveLetter D | Format-Volume
+
+
+
