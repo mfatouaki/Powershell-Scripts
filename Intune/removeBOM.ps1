f85281d9b35daf3877efd77c04a833beb1fc6019
Mehdi Fatouaki <123072235+mfatouaki@users.noreply.github.com>
2023-06-13 16:00:27 +0100

    Add files via upload

---
+++ Intune/removeBOM.ps1
@@ -0,0 +1,13 @@
+﻿function start-removeBOM {
+param(
+    [parameter(Mandatory)]
+    [string]$inputFile 
+)
+$inputFile         = $inputFile -replace '"'
+$MyRawString       = Get-Content -Raw "$inputFile"
+$Utf8NoBomEncoding = New-Object System.Text.UTF8Encoding $False
+$OutputLocation    = "$(Split-Path $inputFile)\$(((Split-Path $inputFile -Leaf) -split "\.")[0])_NoBOM.ps1"
+[System.IO.File]::WriteAllLines($OutputLocation, $MyRawString, $Utf8NoBomEncoding)
+}
+
+start-removeBOM 
